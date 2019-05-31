8229e4c71073fb6c1649ae1e14bccdef31fd0be3
adia <adilamtnez@gmail.com>
2019-05-31 01:03:02 -0300

    Create Model for Categoria and AnaliticTask

--- visualizador/appdigilib/models.py
+++ visualizador/appdigilib/models.py
@@ -2,16 +2,23 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Categoria(models.Model):
+    categoria= models.CharField(max_length=200)
 
+class AnaliticTask(models.Model):
+    task = models.CharField(max_length=250)
 
 class Articulo(models.Model):
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    task = models.ForeignKey(AnaliticTask, on_delete=models.CASCADE)
+    autor = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    doi = models.CharField(max_length=250)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
+    #created_date = models.DateTimeField(
+    #        default=timezone.now)
     published_date = models.DateTimeField(
-            blank=True, null=True)
+            blank=True, null=True, unique_for_year=True)
 
     def publish(self):
         self.published_date = timezone.now()
