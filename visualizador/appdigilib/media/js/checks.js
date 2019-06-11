7f73441706c82bd9904f489a8f70d0c23a34ff51
adia <adilamtnez@gmail.com>
2019-06-10 23:38:56 -0300

    Working Check

--- visualizador/appdigilib/media/js/checks.js
+++ visualizador/appdigilib/media/js/checks.js
@@ -1,15 +1,17 @@
 
+$("#check1").onclick(function (){
+    if( $("#check1").is(':checked'))
+        alert('si');
+    else
+        alert('no');
+});
 
-var aChk = document.getElementsByName('check');
-
-function verificaChecks() {
-
-    for (var i=0;i<aChk.length;i++){
-
-        if (aChk[i].checked){
-            //Aqui eu teria que atribuir '1'
-            categoria = aChk[i].value;
-            alert(aChk[i].id + ' = ' + aChk[i].value + " marcado.");
-        }
-    }
-}
+$(document).ready(function comprobar() {
+   $(document).on('click', '#check1', function () {
+      if($(this).is(":checked")) {
+         alert('Sim');
+      } else {
+         alert('Não');
+      }
+   });
+});
--- visualizador/appdigilib/views.py
+++ visualizador/appdigilib/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 import urllib3
 import json as JSON
 from django.views.generic import ListView
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .forms import *
 from django.template import RequestContext
 from appdigilib.models import Articulo, Categoria, AnaliticTask, Image
@@ -17,12 +17,58 @@ def listado(request):
     tareas = AnaliticTask.objects.all()
     articulos = Articulo.objects.all().order_by('image__articulo__published_date')
     imagenes= Image.objects.all().order_by('articulo')
+
     #file = request.FILES
     #imag = PIL.Image.open('images/index.jpeg')
 #    imag.load()
 #    imag.split()
-
     return render(request, 'list/index_list.html', {'articulos': articulos, 'categorias': categorias, 'tareas': tareas, 'imagenes':imagenes})
+
+
+#Comprobar articulos
+def Lista_Articulo(categoria, articulos_actuales):
+    articulos_actuales = Articulo(articulos_actuales)
+    categoria_eliminada = Categoria(categoria)
+
+    for a in articulos_actuales:
+        if((CateSerach(a,categoria_eliminada)) & (CantCategorias(a)==1)):
+            articulos_actuales = articulos_actuales.delete(a)
+
+    return articulos_actuales
+
+
+#Busca si un articulo tiene una determinada categorìas
+def CateSerach(sarticulo, scategoria):
+    myarticulo= Articulo(sarticulo)
+    buscar_categoria= Categoria(scategoria)
+    list_cat_art = myarticulo.categoria.all()
+
+    for c in list_cat_art:
+        if(c == buscar_categoria):
+          return True
+    return False
+
+#Busca si un artìculo tiene determinada tareas analìticas
+def TaskSearch(sarticulo, stask):
+    myarticulo = Articulo(sarticulo)
+    busca_task = AnaliticTask(stask)
+    list_task_art = myarticulo.task.all()
+
+    for t in list_task_art:
+        if t== busca_task:
+            return True
+    return False
+
+#Contar categoria de un articulo determinado
+def CantCategorias(articulo):
+    myarticulo= Articulo(articulo)
+    return myarticulo.categoria.all().count()
+
+
+#Contar tareas analitica de un articulo determinado
+def CantTareas(articulo):
+    myarticulo = Articulo(articulo)
+    return myarticulo.task.all().count()
 
 
 #Insertar con formulario
@@ -58,5 +104,5 @@ def AdicionarImagen(request):
     return render(request, 'list/article.html', {'form': form, 'categorias': categorias, 'tareas': tareas})
 
 
-def error(self):
-    return ('Arquivo com problema')
+def error(request):
+    return HttpResponse("Algo deu errado")
