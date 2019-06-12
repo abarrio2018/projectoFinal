87acfeaaf0faea584e042c2c90039b9f5f80b426
adia <adilamtnez@gmail.com>
2019-06-12 01:34:49 -0300

    Jscript

--- visualizador/appdigilib/media/js/checks.js
+++ visualizador/appdigilib/media/js/checks.js
@@ -4,31 +4,15 @@ $("input:checkbox.check").each(function (){
     alert(list_check);
 });
 
-$(document).ready(function comprobar() {
-
-      if($(this).is(":checked")) {
-         alert('Sim');
-      } else {
-          mycheck= $(this).text();
-          alert(mycheck);
+$(document).click(function comprobar() {
+    if($(this).is(':checked')) {
+        alert('Sim');
       }
-   });
+    else {
+        alert('no')
+        mycheck= $(this).val.val();
+        alert(mycheck);
+      }
 });
 
-"""$('#btn1').click(function() {
-    console.log("click");
-    a=$('#inputA').val();
-    b=$('#inputB').val();
-    $.ajax({
-        method: 'POST',
-        url: 'ruta de tu vista definida en django'
-        data: {
-            'a': a,
-            'b': b, 
-        },
-        dataType: "json",
-        success: function(response) {
-            $('#outputR').val(response.resultado);
-        }
-    }); 
-});"""
+
