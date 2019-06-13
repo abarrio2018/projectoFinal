2e3377aed85f22ac3157e93126cbe1e7a296195d
adia <adilamtnez@gmail.com>
2019-06-12 23:54:10 -0300

    Paper images

--- visualizador/appdigilib/media/js/checks.js
+++ visualizador/appdigilib/media/js/checks.js
@@ -1,18 +1,12 @@
 
-$("input:checkbox.check").each(function (){
-    var list_check =($(this).checked ? $(this).val():"");
-    alert(list_check);
-});
-
 $(document).click(function comprobar() {
     if($(this).is(':checked')) {
         alert('Sim');
       }
     else {
         alert('no')
-        mycheck= $(this).val.val();
+        mycheck= $(this).text($(this));
         alert(mycheck);
       }
 });
-
 
