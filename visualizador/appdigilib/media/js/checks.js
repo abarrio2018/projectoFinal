401d7234c4fcf4caad82acdeee2102f8f6ee8558
adia <adilamtnez@gmail.com>
2019-06-13 02:23:45 -0300

    With Ajax

--- visualizador/appdigilib/media/js/checks.js
+++ visualizador/appdigilib/media/js/checks.js
@@ -1,13 +1,25 @@
-
-
 $(document).ready(function() {
 
     $('.check').change(function() {
-        if(this.checked) {
-            
-            alert("si")
-        }else{
-          alert("no")
-        }
+
+        $( '.check' ).each(function( index ) {
+            var lista_check = [];
+            if(this.checked) {
+                alert("si")
+            }
+            else{
+                alert("no");
+                var lista_check = lista_check.add( $(this).val());
+
+                $.ajax({
+                    type:"POST",
+                    url:"index/",
+                    data: lista_check
+                });
+
+
+            }
+            alert(lista_check);
+        });
     });
-});
+})
--- visualizador/appdigilib/url.py
+++ visualizador/appdigilib/url.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
 
     path('Imagem/nova', AdicionarImagen, name ='nova_img'),
+    path('index',views.listado, name='post_list'),
 
 ]
 if settings.DEBUG:
