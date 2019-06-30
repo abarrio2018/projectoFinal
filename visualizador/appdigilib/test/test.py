82749a94c23133526a711ee60b7152a10adc616f
adia <adilamtnez@gmail.com>
2019-06-29 21:06:57 -0300

    Add test

---
+++ visualizador/appdigilib/test/test.py
@@ -0,0 +1,13 @@
+"""
+Create by abarrio
+Date: 08/04/2019
+"""
+from django.test import TestCase
+"""The assertEqual() to check for an expected result; 
+    assertTrue() or assertFalse() to verify a condition; 
+    assertRaises() to verify that a specific exception gets raised. 
+    ASSERT statement so the test runner can accumulate all test results and produce a report.
+    setUp() and tearDown() methods allow you to define instructions that will be executed before and after each test method. 
+"""
+
+import unittest
